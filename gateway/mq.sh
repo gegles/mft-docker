a123dbb0a048602935a85046ad88df90e480f46a
Guillaume Egles <gegles@us.ibm.com>
2019-02-13 11:32:25 -0800

    Progress

--- gateway/mq.sh
+++ gateway/mq.sh
@@ -4,5 +4,5 @@
 killall fasp.io-gateway
 rm -rf ~/logs/mq
 mkdir -p ~/logs/mq
-~/mft-docker/gateway/fasp.io-gateway --config ~/mft-docker/gateway/mq/$1-in.toml &
-~/mft-docker/gateway/fasp.io-gateway --config ~/mft-docker/gateway/mq/$1-out.toml &
+~/mft-docker/gateway/fasp.io-gateway --config ~/mft-docker/gateway/mq/${1:-fasp}/$(hostname)-in.toml &
+~/mft-docker/gateway/fasp.io-gateway --config ~/mft-docker/gateway/mq/${1:-fasp}/$(hostname)-out.toml &
